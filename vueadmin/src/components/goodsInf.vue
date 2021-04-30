de4ae998a1e8909947cd220b79701a18e31a4d20
webxrj <webxrj@gmail.com>
2021-04-30 22:59:52 +0800

    fix:微调表格

--- vueadmin/src/components/goodsInf.vue
+++ vueadmin/src/components/goodsInf.vue
@@ -1,30 +1,37 @@
 <template>
-  <div style="width:650px">
+  <div>
     <div v-if="this.$router.currentRoute.path == '/goodsInf'">
       <router-link to="/goodsInf/addgoods">
         <el-button type="primary">添加商品</el-button>
       </router-link>
-      <el-table :data="tableData" height="500">
-        <el-table-column align="center" prop="id" label="id" width="60">
+      <el-table
+        :data="tableData"
+        row-style="height:50px"
+        class="show_table"
+        :key="itemKey"
+        height="400"
+        border="1px solid transparent"
+      >
+        <el-table-column align="center" prop="id" label="id" width="80">
         </el-table-column>
-        <el-table-column align="center" prop="type" label="类型" width="60">
+        <el-table-column align="center" prop="type" label="类型" width="120">
         </el-table-column>
-        <el-table-column align="center" prop="name" label="名称" width="100">
+        <el-table-column align="center" prop="name" label="名称" width="120">
         </el-table-column>
-        <el-table-column prop="count" align="center" label="库存" width="80">
+        <el-table-column prop="count" align="center" label="库存" width="100">
         </el-table-column>
-        <el-table-column label="照片" align="center" width="80">
+        <el-table-column label="照片" align="center" width="200">
           <template slot-scope="scope">
             <img width="80" :src="tableData[scope.$index].image" alt="" />
           </template>
         </el-table-column>
-        <el-table-column prop="price" label="价格" align="center" width="80">
+        <el-table-column prop="price" label="价格" align="center" width="100">
         </el-table-column>
         <el-table-column
           label="操作"
           style="textAligan"
           align="center"
-          width="150"
+          width="170"
         >
           <template slot-scope="scope">
             <el-button
@@ -74,4 +81,12 @@ export default {
   },
 };
 </script>
-<style scoped></style>
+<style scoped>
+.show_table {
+  position: relative;
+  width: 100%;
+  height: 400px;
+  border: none;
+  overflow: auto;
+}
+</style>
